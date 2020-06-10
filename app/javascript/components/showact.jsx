cb9787498f3d5f6e870c59723d254ee7ffadf0b7
lekhweemeng <lek.hweemeng@live.com>
2020-06-10 17:02:13 +0800

    updated and able to show activities in each of the destination

--- app/javascript/components/showact.jsx
+++ app/javascript/components/showact.jsx
@@ -3,13 +3,42 @@ import Container from '@material-ui/core/Container'
 import axios from 'axios';
 
 class Showact extends React.Component {
+    constructor(){
+        super();
+
+        this.state ={
+            activities:[]
+        }
+    }
+
+    componentDidMount(){
+        console.log('component did mount runs')
+          const url = '/activities.json';
+          const runWhenDone = (response) => {
+            const data = response.data
+            console.log("**************")
+            console.log("**************")
+            console.log("**************")
+            console.log("**************")
+            console.log( data );
+            this.setState({ activities: data })
+          }
+          const whenError = (error) => {
+              console.log("eerror", error)
+          }
+          axios.get(url).then(runWhenDone).catch(whenError)
+        }
 
     render(){
-        return (
-            <Container>
-                <p>This is showact</p>
-            </Container>
-            );
+        const activities = this.state.activities.map((activity, index)=>{
+            console.log("this is activity: ", activity)
+            return(<Container >
+                     <strong>{activity.activity}</strong>
+                     <p>Details: {activity.details}</p>
+                     <p>Reference Links: {activity.links}</p>
+                   </Container>)
+        })
+        return (<div>{activities}</div>);
     }
 }
 
--- app/views/activities/_activity.json.jbuilder
+++ app/views/activities/_activity.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! activity, :id, :type, :details, :links, :destination_id, :created_at, :updated_at
+json.extract! activity, :id, :activity, :details, :links, :destination_id, :created_at, :updated_at
 json.url activity_url(activity, format: :json)
