3367a4f0a1e851a450f34f4fc9998875d0f8d834
lekhweemeng <lek.hweemeng@live.com>
2020-06-15 17:14:08 +0800

    added in logic to validate the logged in of user

--- app/javascript/components/logout.jsx
+++ app/javascript/components/logout.jsx
@@ -6,9 +6,19 @@ import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 class Logout extends React.Component {
 
+    logout(){
+        console.log("logout button clicked and the id of this loc is: ", this.props.id)
+        const link = document.createElement('a');
+        link.setAttribute('href', '/users/sign_out');
+        link.setAttribute('rel', 'nofollow');
+        link.setAttribute('data-method', 'delete');
+        document.body.appendChild(link);
+        link.click();
+    }
+
     render(){
         return (
-            <ExitToAppIcon color="primary"/>
+            <ExitToAppIcon color="primary" onClick = {(event)=>{this.logout(event)}}/>
             );
     }
 }
